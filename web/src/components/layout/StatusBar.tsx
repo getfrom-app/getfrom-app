894337aa24d1e858ac582c95d6b3bba732f847d5
Alberto Lezaun <albertolezaun@me.com>
2026-05-21 09:06:08 +0200

    chore: bump version v1.3

--- web/src/components/layout/StatusBar.tsx
+++ web/src/components/layout/StatusBar.tsx
@@ -2,7 +2,7 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { useStore } from '../../store/nodeStore'
 
 // Versión del build web — incrementar en cada deploy significativo
-export const WEB_VERSION = 'v1.2'
+export const WEB_VERSION = 'v1.3'
 
 interface StatusBarProps {
   isSyncing: boolean
